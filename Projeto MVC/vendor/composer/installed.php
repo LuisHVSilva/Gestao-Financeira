67adaa454335548044b4d4fd9745cae24f7bafed
LuisHVSilva <95083203+LuisHVSilva@users.noreply.github.com>
2022-02-17 22:58:21 -0300

    Composer
    Criando e arrumando composer e as camadas mvc

---
+++ Projeto MVC/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '59570f49a1e170fe5cc60d2495e1c0adb56aa29f',
+        'name' => 'luis/financas',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'luis/financas' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '59570f49a1e170fe5cc60d2495e1c0adb56aa29f',
+            'dev_requirement' => false,
+        ),
+    ),
+);
