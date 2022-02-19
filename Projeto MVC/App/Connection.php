3ddb020f2faf25dd9ca64f808142123fe552614e
LuisHVSilva <95083203+LuisHVSilva@users.noreply.github.com>
2022-02-19 08:48:38 -0300

    Model
    Criando camada Model para executar a busca de dados no banco mysql com o Connection  para executar a conexão

---
+++ Projeto MVC/App/Connection.php
@@ -0,0 +1,30 @@
+<?php
+    namespace App;
+
+//Criando classe para conexão com o banco de dados.
+class Connection{        
+    
+    //Criando métodos públicos para crud
+    public static function getDb(){
+
+        $host = "localhost:3312";
+        $dbname = "mvc;charset=utf8";
+        $user = "root";
+        $pass = "";    
+
+        try{
+            $conexao = new \PDO(
+                "mysql:host=".$host.";dbname=".$dbname,
+                $user,
+                $pass
+            );
+
+            return $conexao;
+
+        }catch(\PDOException $e){
+            echo '<p>' .$e->getMessage().'</p>';
+        };
+    }
+};
+
+?>
--- Projeto MVC/App/Controllers/IndexController.php
+++ Projeto MVC/App/Controllers/IndexController.php
@@ -1,6 +1,8 @@
 <?php
     namespace App\Controllers;
     use MF\Controller\Action;
+    use App\Connection;
+    use App\Models\Produto;
 
     //Controllador para tratar o que deve acontecer dentro de cada página
     class IndexController extends Action{
@@ -8,7 +10,17 @@
        
         public function index(){
             
-            $this->view->dados = array('sofa', 'cadeira', 'cama');
+            //$this->view->dados = array('sofa', 'cadeira', 'cama');
+
+            //Instância de conexão
+            $conn = Connection::getDb();
+            
+            //Instância de modelo
+            $produto = new Produto($conn);
+
+            $produtos = $produto->getProdutos();
+            $this->view->dados = $produtos;
+
             //Chamando o conteudo da página que deverá ser acessado pelo index.
             //Primeiro parâmetro -> dados que deverão aparecer na página
             //Segundo parâmetro -> Quais as caractéristicas padrões devem ser obedecidas
@@ -17,7 +29,7 @@
 
         public function sobreNos(){
             
-            $this->view->dados = array('sofa', 'cadeira');
+            //$this->view->dados = array('sofa', 'cadeira');
             //Chamando o conteudo da página que deverá ser acessado pelo sobre_nos
             //Primeiro parâmetro -> dados que deverão aparecer na página
             //Segundo parâmetro -> Quais as caractéristicas padrões devem ser obedecidas
